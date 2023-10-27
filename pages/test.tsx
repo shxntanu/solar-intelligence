7291e5fb68492859a1acab0013216cad2c0297c7
Shantanu <shantanuwable2003@gmail.com>
2023-10-28 01:54:45 +0530

    Page for testing graphs

--- pages/test.tsx
+++ pages/test.tsx
@@ -7,9 +7,13 @@ import {
     Input,
     Button,
     Image,
+    Dropdown,
+    DropdownTrigger,
+    DropdownMenu,
+    DropdownItem,
 } from "@nextui-org/react";
 import Link from "next/link";
-import { useRouter } from "next/router";
+
 import {
     Chart as ChartJS,
     CategoryScale,
@@ -20,22 +24,53 @@ import {
     Legend,
 } from "chart.js";
 import { Bar } from "react-chartjs-2";
-import faker from "faker";
 import CardHolder from "@/components/CardHolder";
+import { convertJSONResponse } from "@/config/functions/conversion";
+import { convertResponseToFormattedData } from "@/config/functions/conversion-v2";
+
+import * as globalRes from "@/constants/global.json";
+import * as regionRes from "@/constants/regions.json";
+import * as numericRes from "@/constants/numeric.json";
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+export const options = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: "top" as const,
+        },
+        title: {
+            display: true,
+            text: "Chart.js Bar Chart",
+        },
+    },
+};
+
+export const data = convertResponseToFormattedData(globalRes);
 
 function RenderingInIFrame() {
+    // const [dataset, setDataset] = useState(data.datasets);
+    // const [labels, setLabels] = useState(data.labels);
+
+    const [dropdowns, setDropdowns] = useState([
+        {
+            
+        }
+    ])
+
     return (
         <div>
-            <div className="flex-grow flex items-center justify-center">
+            <div className="flex-grow flex items-center justify-center py-10">
                 <Card className="w-[400px]">
                     <CardHeader className="flex flex-col gap-3 pt-10">
-                        <Image
-                            alt="nextui logo"
-                            height={40}
-                            radius="sm"
-                            src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
-                            width={40}
-                        />
                         <div className="flex flex-col text-center">
                             <p className="text-md">PageTalk</p>
                             <p className="text-small text-default-500">
@@ -72,26 +107,15 @@ function RenderingInIFrame() {
                                 <Button color="primary" type="submit">
                                     <p className="text-white">Sign In</p>
                                 </Button>
-                                <Link href="/">
-                                    <p className="text-small text-default-500">
-                                        Forgot password?
-                                    </p>
-                                </Link>
-                                <p
-                                    className="text-small text-primary hover:cursor-pointer"
-                                    // onClick={handleSignIn}
-                                >
-                                    New User? Sign Up
-                                </p>
                             </CardBody>
                         </form>
                     </div>
                 </Card>
             </div>
             <div>
-                {/* <CardHolder>
-                    <Bar options={options} data={data} className="max-w-lg" />
-                </CardHolder> */}
+                <CardHolder>
+                    <Bar options={options} data={data} className="" />
+                </CardHolder>
             </div>
         </div>
     );
