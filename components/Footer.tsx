ffba9285fddbf33a6497fdb45f1000aec68dd287
Shantanu <shantanuwable2003@gmail.com>
2024-03-30 15:27:59 +0530

    feat: edit name of tean

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,28 +1,28 @@
 import React from "react";
 
 function Footer() {
-  return (
-    <footer
-      className="bg-white rounded-lg shadow m-4 dark:bg-gray-800"
-      style={{ position: "static", bottom: "0", left: "0", right: "0" }}
-    >
-      <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2023 Hacktastic4™. All Rights Reserved.
-        </span>
-        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-          <li>
-            <a
-              href="https://github.com/orgs/hacktastic-4/repositories"
-              className="hover:underline"
-            >
-              About
-            </a>
-          </li>
-        </ul>
-      </div>
-    </footer>
-  );
+    return (
+        <footer
+            className="bg-white rounded-lg shadow m-4 dark:bg-gray-800"
+            style={{ position: "static", bottom: "0", left: "0", right: "0" }}
+        >
+            <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+                    © 2024 yarn dev. All Rights Reserved.
+                </span>
+                <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+                    <li>
+                        <a
+                            href="https://github.com/orgs/hacktastic-4/repositories"
+                            className="hover:underline"
+                        >
+                            About
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;
