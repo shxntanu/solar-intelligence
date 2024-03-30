8ef41204ff988f80c23f2032f1bf1db1b9253768
Shantanu <shantanuwable2003@gmail.com>
2024-03-30 07:36:51 +0530

    feat: add some accordions

--- pages/surya-sahayak.tsx
+++ pages/surya-sahayak.tsx
@@ -1,12 +1,13 @@
 import { Card, Button, Input } from "@nextui-org/react";
+import Accordion from "@/components/Accordion";
 
 export default function Information() {
     return (
         <div
-            style={{ height: "calc(100vh - 150px)" }}
+            // style={{ height: "calc(100vh - 150px)" }}
             className="flex items-start justify-center pt-5"
         >
-            <div className="space-y-5 max-w-3xl">
+            <div className="space-y-5 w-1/2">
                 <h1 className="pt-5 font-bold text-3xl -pb-1 flex items-center justify-center">
                     Surya Sahayak
                 </h1>
@@ -22,6 +23,11 @@ export default function Information() {
                 <Card className="p-5">
                     <p>Waiting for prompt...</p>
                 </Card>
+                <Accordion />
+                <Accordion />
+
+                <Accordion />
+                <Accordion />
             </div>
         </div>
     );
