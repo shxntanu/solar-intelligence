ef99fdfc872941740b85ae4bc43f59555c216774
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2024-03-30 12:15:28 +0530

    commit v2

--- pages/api/proxy.ts
+++ pages/api/proxy.ts
@@ -2,31 +2,31 @@ import axios, { AxiosResponse } from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
-    try {
-        if (req.method == "POST") {
-            const { type, src, query } = JSON.parse(req.body);
+  try {
+    if (req.method == "POST") {
+      const { type, src, query } = JSON.parse(req.body);
 
-            axios
-                .get(
-                    `https://a3ca-103-97-166-170.ngrok-free.app/?type=${type}&src=${src}&query=${query}`,
-                    // `https://a3ca-103-97-166-170.ngrok-free.app/?type=llm&src=en&query=hello`,
-                    {
-                        headers: {
-                            "ngrok-skip-browser-warning": "true",
-                        },
-                    }
-                )
-                .then((response: AxiosResponse) => {
-                    res.status(response.status).json(response.data);
-                });
-        } else {
-            res.status(405).json({ error: "Method Not Allowed" });
-        }
-    } catch (error) {
-        console.error("Error proxying request:", error);
-        res.status(500).json({ error: "Internal Server Error" });
+      axios
+        .get(
+          `https://a28f-115-248-146-117.ngrok-free.app/?type=${type}&src=${src}&query=${query}`,
+          // `https://a3ca-103-97-166-170.ngrok-free.app/?type=llm&src=en&query=hello`,
+          {
+            headers: {
+              "ngrok-skip-browser-warning": "true",
+            },
+          }
+        )
+        .then((response: AxiosResponse) => {
+          res.status(response.status).json(response.data);
+        });
+    } else {
+      res.status(405).json({ error: "Method Not Allowed" });
     }
+  } catch (error) {
+    console.error("Error proxying request:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 }
--- pages/surya-sahayak.tsx
+++ pages/surya-sahayak.tsx
@@ -3,82 +3,81 @@ import Accordion from "@/components/Accordion";
 import { useState } from "react";
 
 export default function Information() {
-    const [prompt, setPrompt] = useState("");
-    const [response, setResponse] = useState(undefined);
+  const [prompt, setPrompt] = useState("");
+  const [response, setResponse] = useState(undefined);
 
-    return (
-        <div
-            // style={{ height: "calc(100vh - 150px)" }}
-            className="flex items-start justify-center pt-5"
-        >
-            <div className="space-y-5 w-1/2">
-                <h1 className="pt-5 font-bold text-3xl -pb-1 flex items-center justify-center">
-                    Surya Sahayak
-                </h1>
-                <p className="flex items-center justify-center text-center">
-                    This is where you can converse with our custom trained LLM,
-                    complete with the latest <br /> data regarding renewable
-                    resources
-                </p>
-                <div className="flex flex-row space-x-5">
-                    <Input
-                        placeholder="Enter your prompt..."
-                        onChange={(e) => setPrompt(e.target.value)}
-                    />
-                    <Button
-                        variant="bordered"
-                        onClick={() => {
-                            fetch(
-                                // `https://a3ca-103-97-166-170.ngrok-free.app/` +
-                                //     new URLSearchParams({
-                                //         type: "llm",
-                                //         src: "en",
-                                //         query: prompt,
-                                //     }),
-                                // {
-                                //     method: "GET",
-                                //     headers: {
-                                //         "ngrok-skip-browser-warning": "true",
-                                //     },
-                                // }
-                                // {
-                                //     method: "POST",
-                                //     body: JSON.stringify({
-                                //         type: "llm",
-                                //         src: "en",
-                                //         query: prompt,
-                                //     }),
-                                // }
-                                "/api/proxy",
-                                {
-                                    method: "POST",
-                                    body: JSON.stringify({
-                                        type: "llm",
-                                        src: "mr",
-                                        query: prompt,
-                                    }),
-                                }
-                            ).then((res) => {
-                                console.log(res);
-                                res.json().then((data) => {
-                                    console.log(data);
-                                    setResponse(data.response);
-                                });
-                            });
-                        }}
-                    >
-                        Submit
-                    </Button>
-                </div>
-                <Card className="p-5">
-                    <p>{response ?? "Waiting for prompt..."}</p>
-                </Card>
-                <Accordion />
-                <Accordion />
-
-                <Accordion />
-                <Accordion />
-            </div>
+  return (
+    <div
+      // style={{ height: "calc(100vh - 150px)" }}
+      className="flex items-start justify-center pt-5"
+    >
+      <div className="space-y-5 w-1/2">
+        <h1 className="pt-5 font-bold text-3xl -pb-1 flex items-center justify-center">
+          Surya Sahayak
+        </h1>
+        <p className="flex items-center justify-center text-center">
+          This is where you can converse with our custom trained LLM, complete
+          with the latest <br /> data regarding renewable resources
+        </p>
+        <div className="flex flex-row space-x-5">
+          <Input
+            placeholder="Enter your prompt..."
+            onChange={(e) => setPrompt(e.target.value)}
+          />
+          <Button
+            variant="bordered"
+            onClick={() => {
+              fetch(
+                // `https://a3ca-103-97-166-170.ngrok-free.app/` +
+                //     new URLSearchParams({
+                //         type: "llm",
+                //         src: "en",
+                //         query: prompt,
+                //     }),
+                // {
+                //     method: "GET",
+                //     headers: {
+                //         "ngrok-skip-browser-warning": "true",
+                //     },
+                // }
+                // {
+                //     method: "POST",
+                //     body: JSON.stringify({
+                //         type: "llm",
+                //         src: "en",
+                //         query: prompt,
+                //     }),
+                // }
+                "/api/proxy",
+                {
+                  method: "POST",
+                  body: JSON.stringify({
+                    type: "llm",
+                    src: "en",
+                    query: prompt,
+                  }),
+                }
+              ).then((res) => {
+                console.log(res);
+                res.json().then((data) => {
+                  console.log(data);
+                  setResponse(data.response);
+                });
+              });
+            }}
+          >
+            Submit
+          </Button>
         </div>
-    );
+        <Card className="p-5">
+          <p>{response ?? "Waiting for prompt..."}</p>
+        </Card>
+        {/* <Accordion />
+                <Accordion />
+
+                <Accordion />
+                <Accordion /> */}
+      </div>
+    </div>
+  );
 }
